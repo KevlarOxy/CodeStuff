95ae30d9e29a1f7fef4a0c65b9be5137d5759b61
KevlarOxy <Kevin_Oxland@hotmail.com>
2015-08-26 20:49:59 +0100

    first committed files

---
+++ ZoomIn.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoomIn : MonoBehaviour {
+
+	private Camera eyes;
+	private float defaultFOV;
+	private float FOVstore;
+	
+	public float fieldOfView;
+	public float zoomSpeed = 0.1f;
+	public int minZoom = 10;
+	
+	void Start () {
+		eyes = GetComponent<Camera> ();
+		defaultFOV = eyes.fieldOfView;
+	}
+	
+	void Update () {
+		if (Input.GetButton("Zoom")) {
+			Gozoom();
+		} else {
+			eyes.fieldOfView = defaultFOV;
+			FOVstore = defaultFOV;
+			}
+		}
+	
+	void Gozoom () {
+		if (eyes.fieldOfView >= 20) {
+			eyes.fieldOfView = FOVstore - zoomSpeed;
+			FOVstore = eyes.fieldOfView;
+			} else {
+			FOVstore = eyes.fieldOfView;
+		} 
+	}
+}
---
+++ ZoomInOut.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoomInOut : MonoBehaviour {
+
+	private Camera eyes;
+	private float defaultFOV;
+	private float zoomTime = 1f;
+	private float increment;
+	
+	void Start () {
+		eyes = GetComponent<Camera> ();
+		defaultFOV = eyes.fieldOfView;
+	}
+	
+	void Update () {
+		if (Input.GetButton ("Zoom")) {
+			Zooming();
+		} else {
+			ZoomingOut();
+		}
+	}
+	
+	void Zooming() {
+		if (eyes.fieldOfView >= 40f) {
+			eyes.fieldOfView = increment - zoomTime;
+			increment = eyes.fieldOfView;
+		} else {
+			increment = eyes.fieldOfView;
+		}
+	}
+	
+	void ZoomingOut() {
+		if (eyes.fieldOfView < defaultFOV) {
+			eyes.fieldOfView = increment + (zoomTime * 3);
+			increment = eyes.fieldOfView;
+		}
+	}
+}
+
